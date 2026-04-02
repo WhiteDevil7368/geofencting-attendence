5964f3429bb74baf8b8ee6487fb4fa55740efbd5
Satyam Kumar <113131467+WhiteDevil7368@users.noreply.github.com>
2026-04-02 23:37:56 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('attendance-cache-v1').then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
